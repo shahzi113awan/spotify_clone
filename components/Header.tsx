8ae4fb4c18aad4a5739ecae795aaec8cfea31405
shahzaib <shahzi113awan@gmail.com>
2023-10-08 23:42:27 +0500

    divs

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,11 +14,13 @@ const Header = (props: Props) => {
     <div className={twMerge('h-fit bg-gradient-to-b from-emerald-800 p-6')}>
       <div className="w-full mb-4 flex items-center justify-center">
             <div className="hidden md:flex gap-x-2 items-center">
-              <button className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'>
-                <RxCaretLeft className="text-white" size={35}/>
+              <button  className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'>
+                <RxCaretLeft className="text-white" size={35}/>b
+            
               </button>
-              <button className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'><RxCaretRight className="text-white" size={35}/></button>
+              <button className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'><RxCaretRight className="text-white" size={35}/>b</button>
             </div>
+           
       </div>
     </div>
   )
